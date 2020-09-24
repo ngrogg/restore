eb908546b18f281c5f70a79df997978455143965
Nicholas Grogg <Nicholas_grogg6505@hotmail.com>
2020-09-23 16:06:09 -1000

    Started Fedora script

--- fedora_restore.sh
+++ fedora_restore.sh
@@ -1,1 +1,51 @@
 #!/usr/bin/bash
+
+# A BASH script used to restore my Fedora system
+
+## Check if running as root
+if [ "$EUID" -ne 0 ]
+	then echo "Please run as root"
+	    exit
+else
+	echo "Script running as root!"
+fi
+
+echo "Pre-flight all clear"
+echo "Creating LAMP stack"
+echo "Press Control + C to quit at any time"
+
+## Update system and reinstall software
+
+### Update system
+
+### Install software 
+
+### Enable SNAP store 
+
+### Install SNAP store software 
+
+## Clone config repo
+
+### Copy vimrc
+
+### Open vim to trigger installation of plugins 
+
+### Install YouCompleteMe
+
+### Copy ZSHRC file 
+
+## Configure vim wiki and linux academy repo
+
+### Clone vimwiki notes 
+
+#### Create vimwiki directory
+
+#### Copy vimwiki
+
+### Clone linux academy notes repo
+
+#### Copy wikito/wikifrom scripts
+
+#### Add aliases to zshrc
+
+## Restart system for snapd changes to take effect 
