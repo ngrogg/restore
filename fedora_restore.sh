c620d1066c58b7a800c03e92c169e2945878175c
Nicholas Grogg <ngrogg@localhost.localdomain>
2020-11-21 16:55:17 -1000

    Updated script for more general use

--- fedora_restore.sh
+++ fedora_restore.sh
@@ -8,27 +8,32 @@ if [ "$EUID" -ne 0 ]
 	    exit
 else
 	echo "Script running as root!"
+	echo "This is good!"
 fi
 
 echo "Pre-flight all clear"
-echo "Restoring work computer"
+echo "Creating LAMP stack"
 echo "Press Control + C to quit at any time"
 
-## Update system and reinstall software
+## Enter username to configure
+echo "Some configurations are user specific" 
+echo "Enter a username to configure:"
+read username
 
+## Update system and reinstall software
 ### Update system
 echo "Updating system"
 dnf update -y && dnf upgrade -y
 
 ### Install repo software 
-echo "Installing libraries"
+echo "Installing essential software"
 dnf install liberation-fonts-1:2.1.0-1.fc32.noarch python3 qt5 zsh git autokey-gtk redshift cmake gcc-c++ make python3-devel python3-SecretStorage python3-crypto python3-cryptography python3-keyring python3-psutil python3-qt5 python3-requests-kerberos speedtest-cli neofetch gimp -y
 
 ### Install rpm software
 
 #### Download and Install Google Chrome
+cd /home/$username/Downloads
 echo "Downloading and installing Google Chrome"
-cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 rpm -Uhv google-chrome-stable_current_x86_64.rpm
 
@@ -39,79 +44,95 @@ rpm -Uhv nagstamon-3.4.1.fedora32-1.src.rpm
 echo "Nagstamon will still need configured"
 
 ### Enable SNAP classic
+echo "Installing snap store"
 dnf install snapd -y
 ln -s /var/lib/snapd/snap /snap/
 
 ### Install SNAP store software 
+echo "Google Cloud SDK"
 snap install google-cloud-sdk --classic
 
 ### Install non-repo and non-RPM software 
 #### Install MotionPro
 echo "Downloading and installing MotionPro"
-cd ~/Downloads
+cd /home/$username/Downloads
 wget https://www.hipaavault.com/ArrayNetworks/MotionPro_Linux_RedHat_x86-64_1.2.3.sh
 chmod +x MotionPro_Linux_RedHat_x86-64_1.2.3.sh
 bash MotionPro_Linux_RedHat_x86-64_1.2.3.sh
 echo "MotionPro will still need configured"
 
 ## Clone config repo
-cd
-cd ~/Documents/
+echo "Cloning config repo"
+cd /home/$username/Documents/
 mkdir gits
 cd gits
-git clone https://gitlab.com/ngrogg/configs.git
+git clone https://gitlab.com/$username/configs.git
 
 ### Copy vimrc and zshrc
+echo "Copying vimrc and zshrc"
 cd configs 
-cp Desktop\ Configs/vimrc ~/.vimrc
-cp Desktop\ Configs/zshrc ~/.zshrc
+cp Desktop\ Configs/vimrc /home/$username/.vimrc
+cp Desktop\ Configs/zshrc /home/$username/.zshrc
 
 ### Configure zsh
-cd
-mkdir ~/.zsh/
-mkdir ~/.zsh/cache
-touch ~/.zsh/cache/history
-echo "run 'chsh -s $(which zsh)' after script finishes and system reboots"
+echo "Configuring zsh"
+echo "Use tab for fuzzy autocomplete"
 sleep 5
+cd /home/$username
+mkdir /home/$username/.zsh/
+mkdir /home/$username/.zsh/cache
+touch /home/$username/.zsh/cache/history
+sed -i "s/\/home\/$username\:\/usr\/bin\/zsh/\/home\/$username\:\/usr\/bin\/bash/g" /etc/passwd
 
 ### Open vim to trigger installation of plugins 
-cd
+cd /home/$username
 echo "Quit vim after installation finishes"
 sleep 5
 vim .vimrc
 
 ### Install YouCompleteMe
-echo "Installing YouCompleteme"
+echo "Installing YouCompleteMe"
 cd .vim/plugged/YouCompleteMe/
 python3 install.py --clangd-completer
 
 ## Configure vim wiki and linux academy repo
 
-### Clone vimwiki notes and linux academy notes repo
-echo "Cloning notes from git repo"
-cd ~/Documents/gits
+### Clone vimwiki notes repo
+echo "Cloning notes to from git repo"
+cd /home/$username/Documents/gits
 git clone https://github.com/ngrogg360/notes.git
 
 #### Create vimwiki directory
-echo "copying notes to vimwiki directory"
-cd 
+cd /home/$username
 mkdir vimwiki
 
 #### Copy vimwiki
-cp ~/Documents/gits/notes/*.wiki ~/vimwiki
+echo "Copying vimwiki"
+echo "Access vimwiki by opening vi and pressing \ w w"
+cp /home/$username/Documents/gits/notes/*.wiki ~/vimwiki
 
 #### Copy wikito/wikifrom scripts
-echo "Copying scripts to pull from and push to note repo"
-echo "Do not run the push script if you are not ngrogg"
-mkdir ~/.scripts
-cp ~/Documents/gits/la-notes/backupwiki.sh ~/.scripts/wikitogit.sh
-cp ~/Documents/gits/la-notes/restorewiki.sh ~/.scripts/wikifromgit.sh
+echo "Copying scripts from configs"
+mkdir /home/$username/.scripts
+if [ "$username" == "ngrogg" ]; then
+	cp /home/$username/Documents/gits/la-notes/backupwiki.sh ~/.scripts/wikitogit.sh
+	cp /home/$username/Documents/gits/la-notes/restorewiki.sh ~/.scripts/wikifromgit.sh
+else
+	cp /home/$username/Documents/gits/la-notes/restorewiki.sh ~/.scripts/wikifromgit.sh
+fi
 
 #### Add aliases to zshrc
 echo "Adding aliases to zshrc"
-echo "alias wikitogit='bash ~/.scripts/backupwiki.sh'" >> ~/.zshrc
-echo "alias wikifromgit='bash ~/.scripts/restorewiki.sh'" >> ~/.zshrc
+if [ "$username" == "ngrogg" ]; then
+	echo "alias wikitogit='bash ~/.scripts/wikitogit.sh'" >> /home/$username/.zshrc
+	echo "alias wikifromgit='bash ~/.scripts/wikifromgit.sh'" >> /home/$username/.zshrc
+	echo "alias vi=vim" >> /home/$username/.zshrc
+else
+	echo "alias wikifromgit='bash ~/.scripts/wikifromgit.sh'" >> /home/$username/.zshrc
+	echo "alias vi=vim" >> /home/$username/.zshrc
+fi
 
 ## Reboot system
-echo "Rebooting in 10 seconds, don't forget to run 'chsh -s $(which zsh)'"
+echo "Intial configuration complete, system will reboot in 5 seconds"
+sleep 5
 reboot
